8b90bb53f1128c84eed0f264f085de8aeb3c9bb8
harshraj32 <harshraj201999@gmail.com>
2020-05-21 11:58:18 +0530

    basic

---
+++ geolocation/lib/screens/admin_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AdminScreen extends StatefulWidget {
+  @override
+  _AdminScreenState createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
